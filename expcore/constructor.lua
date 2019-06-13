5f3bd6d88090ca920fc6f7278a14fdd43742eeaa
Cooldude2606 <cooldude260607@gmail.com>
2019-06-13 20:37:17 +0100

    Added construtor

--- expcore/constructor.lua
+++ expcore/constructor.lua
@@ -43,6 +43,159 @@
     on_click - function - registers a function as the handler for this event
 ]]
 
-local Construtor = {}
+local Global = require 'utils.global'
+local Event = require 'utils.event'
+require 'table'
 
-return Construtor
+local Construtor = {
+    global = {}, -- constant
+    event = {}, -- constant
+    classes = {},
+    instances = {}
+}
+
+Global.register(Construtor.instances,function(tbl)
+    Construtor.instances = tbl
+end)
+
+local function event_raise_factory(class)
+    local events = class.events
+    return function(name,event)
+        local event_id = events[name]
+        if not event_id then error('Event name '..name..' is not registered to this construct',2) end
+        script.raise_event(event_id,event)
+    end
+end
+
+local function event_handler_factory(event_id)
+    return function(callback)
+        Event.add(event_id,callback)
+    end
+end
+
+local function new_prototype_factory(class)
+    local classes = Construtor.classes
+    local class_id = class._class
+    local prototype = class._prototype
+    local mt = {
+        __call=prototype.__call,
+        __index=prototype
+    }
+    local deapcopy = table.deapcopy
+    local uid_alias = prototype._uid
+    return function()
+        local instance_id = #classes[class._class]+1
+        local rtn = {
+            _class = class_id,
+            _uid = instance_id
+        }
+        if uid_alias then
+            rtn[uid_alias] = instance_id
+        end
+        for key,value in pairs(prototype) do
+            if type(value) ~= 'function' then
+                rtn[key] = deapcopy(value)
+            end
+        end
+        classes[class._class][instance_id] = rtn
+        return setmetatable(rtn,mt)
+    end
+end
+
+local function get_prototype_factory(class)
+    local classes = Construtor.classes
+    return function(instance_id)
+        return classes[class._class][instance_id]
+    end
+end
+
+local function raise_event_prototype_factory(prototype)
+    local events = prototype.events
+    return function(name,event)
+        local handlers = events[name]
+        if handlers then
+            for _,callback in pairs(handlers) do
+                callback(event)
+            end
+        else
+            error('Event name '..name..' is not registered to this prototype',2)
+        end
+    end
+end
+
+local function event_handler_prototype_factory(name,prototype)
+    local events = prototype.events
+    return function(callback)
+        local handlers = events[name]
+        if handlers then
+            handlers[#handlers+1] = callback
+        else
+            error('Event name '..name..' is not registered to this prototype',2)
+        end
+    end
+end
+
+function Construtor.class(data)
+    local classes = Construtor.classes
+    local class_id = #classes+1
+    local rtn = {
+        _class = class_id,
+    }
+
+    for key,value in pairs(data) do
+        if value == Construtor.event then
+            if not rtn.events then
+                rtn.events = {}
+                rtn.raise_event = event_raise_factory(rtn)
+            end
+            rtn.events[key] = script.generate_event_name()
+            rtn[key] = event_handler_factory(rtn.events[key])
+
+        elseif value == Construtor.global then
+            rtn[key] = {}
+            Global.register(rtn[key],function(tbl)
+                rtn[key] = tbl
+            end)
+
+        elseif type(value) == 'table' then
+            local tbl1 = value[1]
+            if tbl1 == Construtor.prototype then
+                local proto = value[2]
+                proto._class = class_id
+                rtn._prototype = proto
+                rtn[key] = proto
+                rtn.new = new_prototype_factory(rtn)
+                rtn.get = get_prototype_factory(rtn)
+            end
+        end
+    end
+
+    classes[class_id] = rtn
+    return rtn
+end
+
+function Construtor.prototype(data)
+    local rtn = {}
+
+    for key,value in pairs(data) do
+        if value == Construtor.event then
+            if not rtn.events then
+                rtn.events = {}
+                rtn.raise_event = raise_event_prototype_factory(rtn)
+            end
+            rtn.events[key] = {}
+            rtn[key] = event_handler_prototype_factory(key,rtn)
+
+        elseif value == Construtor.uid then
+            rtn._uid = key
+        end
+    end
+
+    return {Construtor.prototype,rtn}
+end
+
+return setmetatable(Construtor,{
+    __call=function(self,data)
+        return self.class(data)
+    end
+})
